defc51aadb28a2e7c2022519ad5aaae770bcefb7
Ignacio <ignacio.gomez12@mi.unc.edu.ar>
2024-03-27 23:26:03 -0300

    final test

--- lab1/test.py
+++ lab1/test.py
@@ -1,6 +1,12 @@
 import requests
 
+def print_linea(descripcion):
+    """Imprime una línea con el número de test y su descripción """
+    print(f"---- Test: {descripcion} ----\n")
+
+
 # Obtener todas las películas
+print_linea("obtener todas las películas")
 response = requests.get('http://localhost:5000/peliculas')
 peliculas = response.json()
 print("Películas existentes:")
@@ -9,6 +15,7 @@ for pelicula in peliculas:
 print()
 
 # Agregar una nueva película
+print_linea("Agregar una nueva película")
 nueva_pelicula = {
     'titulo': 'Pelicula de prueba',
     'genero': 'Acción'
@@ -23,6 +30,7 @@ else:
 print()
 
 # Obtener detalles de una película específica
+print_linea("obtener detalles de una película específica")
 id_pelicula = 1  # ID de la película a obtener
 response = requests.get(f'http://localhost:5000/peliculas/{id_pelicula}')
 if response.status_code == 200:
@@ -33,7 +41,9 @@ else:
     print("Error al obtener los detalles de la película.")
 print()
 
+
 # Actualizar los detalles de una película
+print_linea("actualizar los detalles de una película")
 id_pelicula = 1  # ID de la película a actualizar
 datos_actualizados = {
     'titulo': 'Nuevo título',
@@ -48,10 +58,39 @@ else:
     print("Error al actualizar la película.")
 print()
 
+
 # Eliminar una película
+print_linea("eliminar una película")
 id_pelicula = 1  # ID de la película a eliminar
 response = requests.delete(f'http://localhost:5000/peliculas/{id_pelicula}')
 if response.status_code == 200:
     print("Película eliminada correctamente.")
 else:
     print("Error al eliminar la película.")
+print()
+
+# Recomendar película random de cierto genero
+print_linea("recomendar película random de cierto genero")
+response = requests.get('http://localhost:5000/peliculas/recomendarPorGenero/Drama')
+if response.status_code == 200:
+    pelicula_recomendada = response.json()
+    print("Detalles de la película recomendada:")
+    print(f"ID: {pelicula_recomendada['id']}, Título: {pelicula_recomendada['titulo']},")
+else:
+    print("Error al obtener recomendación.")
+print()
+
+ 
+# Recomendar película para el próximo feriado
+print_linea("recomendar película para el próximo feriado")
+genero_pelicula = "Drama"
+response = requests.get(f'http://localhost:5000/peliculas/recomendarFeriado/{genero_pelicula}')
+if response.status_code == 200:
+    pelicula_recomendada = response.json()
+    print("Detalles de la película:")
+    print(f"ID: {pelicula_recomendada['id']}, "
+           + f"Título: {pelicula_recomendada['titulo']}, "
+           + f"Género: {pelicula_recomendada['genero']}, \n"
+           + f"Feriado: {pelicula_recomendada['feriado']}")
+else:
+    print("Error al obtener recomendación.")
