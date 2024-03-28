a2e90ef2c15ad606a0c33aeeb630c3f6c6ee0233
Ignacio <ignacio.gomez12@mi.unc.edu.ar>
2024-03-27 23:25:12 -0300

    proximo_feriado api

--- lab1/proximo_feriado.py
+++ lab1/proximo_feriado.py
@@ -1,20 +1,29 @@
 import requests
 from datetime import date
 
+
 def get_url(year):
     return f"https://nolaborables.com.ar/api/v2/feriados/{year}"
 
-months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+months = ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
+          'Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
 days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
 
 def day_of_week(day, month, year):
     return days[date(year, month, day).weekday()]
+
+def fetch_holidays_tipo(tipo):
+    response = requests.get(get_url(date.today().year), timeout=10)
+    data = response.json()
+    feriados_tipo = list(filter(lambda d: d['tipo'] == tipo, data))
+    return feriados_tipo
 
 class NextHoliday:
     def __init__(self):
         self.loading = True
         self.year = date.today().year
         self.holiday = None
+        self.type = None
 
     def set_next(self, holidays):
         now = date.today()
@@ -30,9 +39,10 @@ class NextHoliday:
 
         self.loading = False
         self.holiday = holiday
-
+        self.type = holiday['tipo']
+        
     def fetch_holidays(self):
-        response = requests.get(get_url(self.year))
+        response = requests.get(get_url(self.year), timeout=10)
         data = response.json()
         self.set_next(data)
 
@@ -49,6 +59,8 @@ class NextHoliday:
             print("Tipo:")
             print(self.holiday['tipo'])
 
-next_holiday = NextHoliday()
-next_holiday.fetch_holidays()
-next_holiday.render()
+#next_holiday = NextHoliday()
+#next_holiday.fetch_holidays()
+#next_holiday.render()
+#print(fetch_holidays_tipo("puente"))
+            
